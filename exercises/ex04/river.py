d426052b31704b99e5e5fa2f9f792d5624d05a91
Payton Emerick <pemerick@unc.edu>
2025-04-09 23:12:56 -0400

    Changes to exercise

--- exercises/ex04/river.py
+++ exercises/ex04/river.py
@@ -2,8 +2,8 @@
 
 __author__: str = "730458530"
 
-from exercises.ex04.fish import Fish
-from exercises.ex04.bear import Bear
+from exercises.EX04.fish import Fish
+from exercises.EX04.bear import Bear
 
 
 class River:
--- exercises/ex04/river_simulation.py
+++ exercises/ex04/river_simulation.py
@@ -2,7 +2,7 @@
 
 __author__: str = "730458530"
 
-from exercises.ex04.river import River
+from exercises.EX04.river import River
 
 my_river = River(num_fish=10, num_bears=2)
 
